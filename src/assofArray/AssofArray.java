dfea3f1847333d5952af0d9b228b7bbbfbdbae77
muhammadmustafa <muhammadmustafa@DESKTOP-PHR9C7R>
2023-01-14 18:09:33 +0500

    Array Ass

---
+++ src/assofArray/AssofArray.java
@@ -0,0 +1,81 @@
+package assofArray;
+
+//import java.util.ArrayList;
+//import java.util.LinkedList;
+
+public class AssofArray {
+
+	public static void main(String[] args) {
+
+		//An ArrayList of Integers and add 5 numbers:
+//
+//		ArrayList<Integer> a = new ArrayList<Integer>();
+//		a.add(1);
+//		a.add(2);
+//		a.add(3);
+//		a.add(4);
+//		a.add(5);
+//		
+//		for(int l : a) {
+//			
+//			System.out.println(l);
+//		}
+
+		
+		//LinkedList of string And Add 5 Name
+//		
+//		LinkedList<String> a = new LinkedList<String>();
+//		a.add("Ali");
+//		a.add("Muhammad");
+//		a.add("Mustafa");
+//		a.add("Khalid");
+//		a.add("Wasi");
+//
+//		for (String b : a) {
+//			
+//			System.out.println(b);
+//		}
+
+		
+		//Allow a User to add and remove elements from an Arraylist
+		
+//		Person p1 = new Person();
+//		p1.setName("Ahmed");
+//		p1.setAge("21");
+//		Person p2 = new Person();
+//		p2.setName("Wasi");
+//		p2.setAge("18");
+//		Person p3 = new Person();
+//		p3.setName("Mustafa");
+//		p3.setAge("23");
+//		Person p4 = new Person();
+//		p4.setName("Khalid");
+//		p4.setAge("29");
+//		Person p5 = new Person();
+//		p5.setName("Ali");
+//		p5.setAge("17");
+//		
+//		ArrayList<Person> l1 = new ArrayList<Person>();
+//		l1.add(p1);
+//		l1.add(p2);
+//		l1.add(p3);
+//		l1.add(p4);
+//		l1.add(p5);
+//		l1.remove(p3);
+//		
+//		for(Person l : l1) {
+//			
+//			System.out.println(l.getName());
+//			System.out.println(l.getAge());
+//		}
+//		
+		
+//Create a program that	Allows a user to search for an element in an arrayList and a LinkedList of integers. Print out the index of the element if it is found, or a message saying that the element is not in the List.
+
+		
+		
+		
+	}
+
+}
+
