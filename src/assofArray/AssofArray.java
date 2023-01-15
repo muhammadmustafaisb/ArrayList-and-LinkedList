0728df3a4106f9f68d80b1202b1d1b6200af0157
muhammadmustafa <muhammadmustafa@DESKTOP-PHR9C7R>
2023-01-15 12:05:50 +0500

    Updated

--- src/assofArray/AssofArray.java
+++ src/assofArray/AssofArray.java
@@ -1,7 +1,8 @@
 package assofArray;
 
-//import java.util.ArrayList;
-//import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class AssofArray {
 
@@ -72,9 +73,79 @@ public class AssofArray {
 		
 //Create a program that	Allows a user to search for an element in an arrayList and a LinkedList of integers. Print out the index of the element if it is found, or a message saying that the element is not in the List.
 
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		a.add(10);
+		a.add(20);
+		a.add(30);
+		a.add(40);
+		a.add(50);
+		a.add(60);
+		a.add(0,1);
+		a.add(1,2);
+		a.add(2,3);
+		a.add(3,4);
+		a.add(4,5);
+		a.add(5,6);
+		a.add(6,7);
+		a.add(7,8);
+		a.add(8,9);
+		a.add(9,19);
+		
+		LinkedList<Integer> b = new LinkedList<Integer>();
+		b.add(70);
+		b.add(80);
+		b.add(90);
+		b.add(100);
+		b.add(110);
+		b.add(120);
+		b.add(0, 11);
+		b.add(1, 10);
+		b.add(2, 9);
+		b.add(3, 8);
+		b.add(4, 7);
+		b.add(5, 6);
+		b.add(6, 5);
+		b.add(7, 4);
+		b.add(8, 3);
+		b.add(9, 2);
+		b.add(10, 1);
 		
 		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter The Number ");
+		int number = scanner.nextInt();
 		
+		int i  = a.indexOf(number);
+		int ind = b.indexOf(number);
+		
+		if ( a.contains(number) == b.contains(number)) 
+		{
+		
+			System.out.println("Element in an arraylist and the idex number is " + i + " And linkedList is " +ind);
+			
+			
+		}
+		
+		else if(a.contains(number)) 
+		{
+
+			System.out.println("Element in an arraylist and the idex number is " + i );
+			
+		}
+
+		else if(b.contains(number)) 
+		{
+		
+			System.out.println("Element in an LinkedList and the idex number is " +ind);
+			
+		}
+		
+		else
+		{
+			
+			System.out.println("The element is not in the List");
+		
+		} 
 	}
 
 }
